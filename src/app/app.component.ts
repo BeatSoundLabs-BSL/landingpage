cf6a98be160f992844ff8b7b66e0a935088cb4b9
Jmondonedor <jmr3811305@gmail.com>
2025-05-10 13:58:59 -0500

    fix(main): fixed imports and added localize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeroComponent} from './landing/components/hero/hero.component';
+import {HowItWorksComponent} from './landing/components/how-it-works/how-it-works.component';
+import {FeaturesComponent} from './landing/components/features/features.component';
+import {CallToActionComponent} from './landing/components/call-to-action/call-to-action.component';
+import {ContactComponent} from './landing/components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroComponent, HowItWorksComponent, FeaturesComponent, CallToActionComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/landing/components/contact/contact.component.ts
+++ src/app/landing/components/contact/contact.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {MatButton} from '@angular/material/button';
 import {MatInput} from '@angular/material/input';
+import {MatFormFieldModule, MatLabel} from '@angular/material/form-field';
+import {MatRadioButton, MatRadioGroup, MatRadioModule} from '@angular/material/radio';
 
 @Component({
   selector: 'app-contact',
   imports: [
     FormsModule,
-    MatFormField,
     MatButton,
-    MatInput
+    MatInput,
+    MatLabel,
+    MatRadioButton,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatRadioGroup
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
