17ae77118c19fa7bca466445e0790f7a2b528f21
JJSON-11110 <millonesjesus@outlook.com>
2025-05-16 01:51:40 -0500

    assets: fixed styles

--- src/app/landing/components/features/features.component.ts
+++ src/app/landing/components/features/features.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatGridList, MatGridTile} from '@angular/material/grid-list';
 import {MatCard, MatCardContent, MatCardTitle} from '@angular/material/card';
 import {NgForOf} from '@angular/common';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-features',
@@ -11,16 +12,33 @@ import {NgForOf} from '@angular/common';
     MatCard,
     MatCardTitle,
     MatCardContent,
-    NgForOf
+    NgForOf,
+    MatIcon
   ],
   templateUrl: './features.component.html',
   styleUrl: './features.component.css'
 })
 export class FeaturesComponent {
   features = [
-    { title: 'Calendario de reservas', desc: 'Lorem ipsum dolor sit amet...' },
-    { title: 'Registros de uso', desc: 'Lorem ipsum dolor sit amet...' },
-    { title: 'Alertas por exceso de ruido', desc: 'Lorem ipsum dolor sit amet...' },
-    { title: 'Búsqueda por cercanía', desc: 'Lorem ipsum dolor sit amet...' },
+    {
+      title: 'Calendario de reservas',
+      desc: 'Sistema intuitivo para gestionar y programar el uso de tus salas de ensayo sin complicaciones.',
+      icon: 'calendar_today'
+    },
+    {
+      title: 'Registros de uso',
+      desc: 'Seguimiento detallado del uso de tus espacios con métricas claras y reportes personalizados.',
+      icon: 'insert_chart'
+    },
+    {
+      title: 'Alertas por exceso de ruido',
+      desc: 'Notificaciones instantáneas cuando se detectan niveles de sonido por encima de los límites establecidos.',
+      icon: 'notifications_active'
+    },
+    {
+      title: 'Búsqueda por cercanía',
+      desc: 'Encuentra fácilmente las salas de ensayo más cercanas a tu ubicación con nuestro sistema de geolocalización.',
+      icon: 'location_on'
+    },
   ];
 }
